dbd2cdca94df643980feb10e01258ecfea87258b
Icescrum-gag <ph.hofbauer@outlook.com>
2020-12-03 09:44:27 +0100

    Server added

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,8 +1,21 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
 
+    public Server()  {
 
+        try{
+            ServerSocket server = new ServerSocket(5555);
+        }catch(IOException e ){
+
+
+         e.printStackTrace();
+
+        }
+    }
 
 
 
